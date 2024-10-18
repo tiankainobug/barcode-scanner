275c17308475823acccde00b7ecc710fc43ca485
tiankai <tiankai@chinatelecom.cn>
2024-10-18 16:11:38 +0800

    333

---
+++ precache-manifest.929771aeb00191323b54541fe6fbdbba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bac9012ef660df9977c",
+    "url": "css/app.f6a58d85.css"
+  },
+  {
+    "revision": "69c60074d781fea7b80d365020c33063",
+    "url": "index.html"
+  },
+  {
+    "revision": "6bac9012ef660df9977c",
+    "url": "js/app.2b16d649.js"
+  },
+  {
+    "revision": "d0aac6f1a6f34d03731b",
+    "url": "js/chunk-vendors.94e03280.js"
+  },
+  {
+    "revision": "0a5f5cd8b59501d25899e0dfdee479a4",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
